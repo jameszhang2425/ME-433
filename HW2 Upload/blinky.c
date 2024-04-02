9ce114351135cca8b3a210bdf6df642802b43462
jameszhang2425 <165177050+jameszhang2425@users.noreply.github.com>
2024-04-02 11:49:28 -0500

    Add files via upload

---
+++ HW2 Upload/blinky.c
@@ -0,0 +1,25 @@
+// James Zhang
+
+#include "pico/stdlib.h"
+
+// The LED is connected to GPIO 25
+#define LED_PIN 25
+
+// Main (runs on core 0)
+int main() {
+    // Initialize the LED pin
+    gpio_init(LED_PIN);
+    // Configure the LED pin as an output
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    // Loop
+    while (true) {
+        // Set high
+        gpio_put(LED_PIN, 1);
+        // Sleep for 100 ms
+        sleep_ms(100);
+        // Set low
+        gpio_put(LED_PIN, 0);
+        // Sleep for 100 ms
+        sleep_ms(100);
+    }
+}
