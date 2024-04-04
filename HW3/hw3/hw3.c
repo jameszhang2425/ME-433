903ad8687fb9c968b056760367145c553d2ab59d
jameszhang2425 <jameszhang2025@u.northwestern.edu>
2024-04-04 01:06:10 -0500

    changes

--- HW3/hw3/hw3.c
+++ HW3/hw3/hw3.c
@@ -9,10 +9,9 @@ int main() {
     }
     printf("Start!\n");
 
-    gpio_init(16); // init pin 16 as output
+    gpio_init(16);
     gpio_set_dir(16, GPIO_OUT);
-
-    gpio_init(18); // init pin 18 as input
+    gpio_init(18); 
     gpio_set_dir(18, GPIO_IN);
 
     adc_init(); // init the adc module
@@ -20,23 +19,17 @@ int main() {
     adc_select_input(0); // select to read from ADC0
 
     while (1) {
-        // turn LED on
         gpio_put(16, 1);
 
-        // wait for button input
         while (gpio_get(18) != 1) {
-            sleep_ms(10);
+            sleep_ms(100);
         }
 
-        // if gotten here, turn LED off
         gpio_put(16, 0);
 
-        // read number of samples requested
         char message[100];
-        int samples;
-        printf("Please enter number of analog samples: \r\n");
-        scanf("%d", &samples);
-        printf("%d samples requested\r\n", samples);
+        scanf("%d", message);
+        printf("sample size: %d\r\n", message);
 
         // reads ADC and prints voltages that number of times
         for (int i = 0; i < samples; i++) {
