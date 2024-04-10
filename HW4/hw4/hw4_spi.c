3a1bcabf713189bd3803de854c5fcc43a55f3439
jameszhang2425 <jameszhang2025@u.northwestern.edu>
2024-04-10 17:47:14 -0500

    updated hw4.c

--- HW4/hw4/hw4_spi.c
+++ HW4/hw4/hw4_spi.c
@@ -9,17 +9,6 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/spi.h"
-/**
-   Connections on Raspberry Pi Pico board and a generic bme280 board, other
-   boards may vary.
-
-   GPIO 16 (pin 21) MISO/spi0_rx-> SDO/SDO on bme280 board
-   GPIO 17 (pin 22) Chip select -> CSB/!CS on bme280 board
-   GPIO 18 (pin 24) SCK/spi0_sclk -> SCL/SCK on bme280 board
-   GPIO 19 (pin 25) MOSI/spi0_tx -> SDA/SDI on bme280 board
-   3.3v (pin 36) -> VCC on bme280 board
-   GND (pin 38)  -> GND on bme280 board
-*/
 
 #define READ_BIT 0x80
 
@@ -51,23 +40,10 @@ static inline void cs_deselect() {
 #if defined(spi_default) && defined(PICO_DEFAULT_SPI_CSN_PIN)
 static void write_register(uint8_t reg, uint8_t data) {
     uint8_t buf[2];
-    buf[0] = reg & 0x7f;  // remove read bit as this is a write
-    buf[1] = data;
+    buf[0] = pin; 
+    buf[1] = initialise;
     cs_select();
     spi_write_blocking(spi_default, buf, 2);
-    cs_deselect();
-    sleep_ms(10);
-}
-
-static void read_registers(uint8_t reg, uint8_t *buf, uint16_t len) {
-    // For this particular device, we send the device the register we want to read
-    // first, then subsequently read from the device. The register is auto incrementing
-    // so we don't need to keep sending the register we want, just the first.
-    reg |= READ_BIT;
-    cs_select();
-    spi_write_blocking(spi_default, &reg, 1);
-    sleep_ms(10);
-    spi_read_blocking(spi_default, 0, buf, len);
     cs_deselect();
     sleep_ms(10);
 }
@@ -77,11 +53,11 @@ static void read_registers(uint8_t reg, uint8_t *buf, uint16_t len) {
 int main() {
     stdio_init_all();
 #if !defined(spi_default) || !defined(PICO_DEFAULT_SPI_SCK_PIN) || !defined(PICO_DEFAULT_SPI_TX_PIN) || !defined(PICO_DEFAULT_SPI_RX_PIN) || !defined(PICO_DEFAULT_SPI_CSN_PIN)
-#warning spi/bme280_spi example requires a board with SPI pins
+#warning spi/mcp4912_spi example requires a board with SPI pins
     puts("Default SPI pins were not defined");
 #else
 
-    printf("Hello, bme280! Reading raw data from registers via SPI...\n");
+    printf("Hello, mcp4912! Reading raw data from registers via SPI...\n");
 
     // This example will use SPI0 at 0.5MHz.
     spi_init(spi_default, 500 * 1000);
@@ -95,12 +71,14 @@ int main() {
     gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
     gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
 
-    // See if SPI is working - interrograte the device for its I2C ID number, should be 0x60
-    uint8_t id;
-    read_registers(0xD0, &id, 1);
-    printf("Chip ID is 0x%x\n", id);
+    while(1){
+        write_register(0x70, 0x00);
+        write_register(0xF0, 0x00);
+        sleep_ms(500);
 
-    write_register(0xF2, 0x1); // Humidity oversampling register - going for x1
-    write_register(0xF4, 0x27);// Set rest of oversampling modes and run mode to normal
+        write_register(0x7F, 0xFF);
+        write_register(0xFF, 0xFF);
+        sleep_ms(500);
+    }
 #endif
 }
