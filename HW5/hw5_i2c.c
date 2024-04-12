fe05cb90d17aef3248ea218b31cd559e803115e8
jameszhang2425 <jameszhang2025@u.northwestern.edu>
2024-04-12 15:34:41 -0500

    updated hw5.c

--- HW5/hw5_i2c.c
+++ HW5/hw5_i2c.c
@@ -10,36 +10,38 @@
 #define REG_GPIO _u(0x09)
 #define REG_OLAT _u(0x0A)
 
+#define LED_PIN 25
 
 void mcp23008_init() {
-    // use the "handheld device dynamic" optimal setting (see datasheet)
     uint8_t buf[2];
 
     // send register number followed by its corresponding value
     buf[0] = REG_IODIR;
     buf[1] = 0b01111111;
     i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
+}
 
-    // osrs_t x1, osrs_p x4, normal mode operation
-    const uint8_t reg_ctrl_meas_val = (0x01 << 5) | (0x03 << 2) | (0x03);
-    buf[0] = REG_CTRL_MEAS;
-    buf[1] = reg_ctrl_meas_val;
+void set_pin(char v) {
+    uint8_t buf[2];
+
+    // send register number followed by its corresponding value
+    buf[0] = REG_OLAT;
+    buf[1] = v<<7;
     i2c_write_blocking(i2c_default, ADDR, buf, 2, false);
 }
 
-void bmp280_read_raw(int32_t* temp, int32_t* pressure) {
-    // BMP280 data registers are auto-incrementing and we have 3 temperature and
-    // pressure registers each, so we start at 0xF7 and read 6 bytes to 0xFC
-    // note: normal mode does not require further ctrl_meas and config register writes
 
-    uint8_t buf[6];
-    uint8_t reg = REG_PRESSURE_MSB;
+unsigned int read_pin() {
+    uint8_t buf[1];
+    uint8_t reg = REG_GPIO;
     i2c_write_blocking(i2c_default, ADDR, &reg, 1, true);  // true to keep master control of bus
-    i2c_read_blocking(i2c_default, ADDR, buf, 6, false);  // false - finished with bus
+    i2c_read_blocking(i2c_default, ADDR, buf, 1, false);  // false - finished with bus
 
-    // store the 20 bit read in a 32 bit signed integer for conversion
-    *pressure = (buf[0] << 12) | (buf[1] << 4) | (buf[2] >> 4);
-    *temp = (buf[3] << 12) | (buf[4] << 4) | (buf[5] >> 4);
+    if (buf[0]&0b1==0b1){
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 
@@ -54,11 +56,17 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    sleep_ms(250); // sleep so that data polling and register update don't collide
     while (1) {
         gpio_put(LED_PIN, 1);
         sleep_ms(100);
         gpio_put(LED_PIN,0);
         sleep_ms(100);
+
+        unsigned int is_button_pressed = read_pin();
+        if (is_button_pressed == 1){
+            set_pin(0);
+        } else if (is_button_pressed == 0){
+            set_pin(1);
+        }
     }
 }
